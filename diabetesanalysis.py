2f044d42b72f1895ccf15d7d187b95440b7c0ad2
YomiFocus <adebogunoriyomi@gmail.com>
2025-06-16 14:08:01 -0700

    Create diabetesanalysis.py

---
+++ diabetesanalysis.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import streamlit as st
+import matplotlib.pylot as pt
+import numpy as np
+import seaborn as sns
+
+
+#read a csv file
+#import csv file
+#convert file to dataframe
+
+
+df = pd.read_csv("diabetes.csv")
+st.markdown("# First Five Items")
+st.write(df.head())
